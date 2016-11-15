50027708cfe897a241aff27f4bc00c1bd2e9e377
Asuka Ochi <asukaochi@gmail.com>
2016-11-15 14:35:04 +0000

    add Station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,17 @@
+require "station"
+
+describe Station do
+  subject(:station){ Station.new(:aldgate_east, 1) }
+
+  it { is_expected.to respond_to :name }
+  it { is_expected.to respond_to :zone }
+
+  it "should return name when access" do
+    expect(station.name).to eq :aldgate_east
+  end
+
+  it "should return zone when access" do
+    expect(station.zone).to eq 1
+  end
+
+end
